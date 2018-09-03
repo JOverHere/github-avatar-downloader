b0bdc4e4a7af9e0e20c0d492abee90d26903fe52
Jenna Woodley <jenatalie@msn.com>
2018-09-03 21:51:08 +0000

    connected functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets');
+var fs = require("fs");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,17 +13,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+
+
   request(options, function(err, res, body) {
     var info = JSON.parse(body);
-    info.forEach(function(item){
-      console.log(item.avatar_url)
-    })
+
     cb(err, info);
   });
 }
 
+
+function downloadImageByURL(url, filepath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode, response.statusMessage, response.headers['content-type']);
+       })
+       .pipe(fs.createWriteStream(filepath));
+}
+
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  //console.log("Result:", result);
+  console.log("Result:", result);
+
+    result.forEach(function(item){
+      downloadImageByURL(item.avatar_url, `avatars/${item.login}.jpg`);
+    })
+
 });
+
+
 

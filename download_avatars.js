d80aef56a544dcda65bb169e3e1a1c82e419e2d2
Jenna Woodley <jenatalie@msn.com>
2018-09-03 21:01:48 +0000

    getting avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -8,17 +8,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': secrets
+      'Authorization': `token ${secrets.GITHUB_TOKEN}`
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var info = JSON.parse(body);
+    info.forEach(function(item){
+      console.log(item.avatar_url)
+    })
+    cb(err, info);
   });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
 });
 
